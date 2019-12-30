06f8a6d8b0fcccaa076727c91e1dd9f7c6d0b3fa
ashwi1234-user <59156365+ashwi1234-user@users.noreply.github.com>
2019-12-30 17:01:31 +0530

    Add files via upload

---
+++ JavaPro/first/src/first1/first1.java
@@ -0,0 +1,21 @@
+package first1;
+
+public class first1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		System.out.println("hello");
+
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		
+
+
+	}
+
+}
